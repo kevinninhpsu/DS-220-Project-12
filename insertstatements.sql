0141bcc192ea04b27a3b5865bc1477799e788501
jck5770 <156126035+jck5770@users.noreply.github.com>
2024-04-05 13:45:42 -0400

    Update insertstatements.sql

--- insertstatements.sql
+++ insertstatements.sql
@@ -1,17 +1,17 @@
-insert into facultyMember values(facultyID(#) , 'facultyname'));
+insert into facultyMember values(facultyID(x) , 'facultyname'));
 
-insert into department values(departmentID(#) , 'department name'));
+insert into department values(departmentID(x) , 'department name'));
 
-insert into vehicle values(vehicleID(#) , 'vehicleType' , odometerStart(#) , odometerEnd(#) , 'maintenanceComplains'(long text) , 'creditCardNumber'));
+insert into vehicle values(vehicleID(x) , 'vehicleType' , odometerStart(x) , odometerEnd(x) , 'maintenanceComplains'(long text) , 'creditCardNumber'));
 
-insert into maintenanceLogForm values(logID(serial),vehicleID(#),'maintenanceDescription'(long text), logEntryDate(xx/xx/xxxx), completeDate(xx/xx/xxxx), 'mechanicInformation'(long text));
+insert into maintenanceLogForm values(logID(serial),vehicleID(x),'maintenanceDescription'(long text), logEntryDate(xx/xx/xxxx), completeDate(xx/xx/xxxx), 'mechanicInformation'(long text));
 
-insert into maintenanceDetailForm values(detailID(serial),logID(#), 'mechanicInformation'(long text), 'partsUsed'(long text), 'maintenanceSummary'(long text), completionStatus(true or false));
+insert into maintenanceDetailForm values(detailID(serial),logID(x), 'mechanicInformation'(long text), 'partsUsed'(long text), 'maintenanceSummary'(long text), completionStatus(true or false));
 
 insert into partsInventory values(partID(serial), 'partName' , 'partQuality'(long text), minimumOnHand(#));
 
-insert into partsUsageForm values(usageID(serial) , logID(#),detailID(#) , partID(#));
+insert into partsUsageForm values(usageID(serial) , logID(x), detailID(x) , partID(x));
 
-insert into TFBS_Center values(tfbsCenterID(serial) , vehicleID(#) , logID(#) , partID(#));
+insert into TFBS_Center values(tfbsCenterID(serial) , vehicleID(x) , logID(x) , partID(x));
 
-insert into monthlyReports values(reportID(serial), month(xx), 'maintenanceSummary'(long text), 'mechanicInformation'(long text), usageID(#), 'mileageReport'(long text), revenue($xx.xx));
+insert into monthlyReports values(reportID(serial), month(xx), 'maintenanceSummary'(long text), 'mechanicInformation'(long text), usageID(x), 'mileageReport'(long text), revenue($xx.xx));
